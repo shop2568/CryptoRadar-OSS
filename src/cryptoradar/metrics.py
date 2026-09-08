37ffde3357e9966f678af8bbb27d6b9195d96cfd
shop2568 <107873015+shop2568@users.noreply.github.com>
2026-09-08 16:35:07 +0800

    Import Iterable from collections.abc

--- src/cryptoradar/metrics.py
+++ src/cryptoradar/metrics.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
 from dataclasses import dataclass
 from math import inf
-from typing import Iterable
 
 
 @dataclass(frozen=True, slots=True)
